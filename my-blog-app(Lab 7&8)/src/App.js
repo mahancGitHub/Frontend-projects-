8913bcdb2bb80af703670956ef49a2fdcfa59064
mahancGitHub <161758744+mahancGitHub@users.noreply.github.com>
2024-02-29 22:04:07 +0100

    Learning React

---
+++ my-blog-app(Lab 7&8)/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css'; 
+import BlogPosts from './BlogPosts';
+
+function App() {
+  return (
+    <div>
+      <BlogPosts />
+    </div>
+  );
+}
+
+export default App;
